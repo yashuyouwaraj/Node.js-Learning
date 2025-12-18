c978b641c15af1ca3904e1c34d7b731c0d61cca0
Yashu Youwaraj <amanyasu@gmail.com>
2025-12-18 10:52:59 +0530

    Nodejs Microservices - Identity service completed

--- 15. Nodejs Microservices/identity-service/src/routes/identity-service.js
+++ 15. Nodejs Microservices/identity-service/src/routes/identity-service.js
@@ -1,9 +1,12 @@
 const express = require("express")
 
-const registerUser = require("../controllers/identity-controller")
+const {registerUser,loginUser, refreshTokenUser,logoutUser} = require("../controllers/identity-controller")
 
 const router = express.Router()
 
 router.post("/register",registerUser)
+router.post("/login",loginUser)
+router.post("/refresh-token",refreshTokenUser)
+router.post("/logout",logoutUser)
 
 module.exports=router;
--- 15. Nodejs Microservices/identity-service/src/utils/validation.js
+++ 15. Nodejs Microservices/identity-service/src/utils/validation.js
@@ -9,4 +9,12 @@ const validateRegistration = (data)=>{
     return schema.validate(data)
 }
 
-module.exports={validateRegistration}
+const validateLogin = (data)=>{
+    const schema = Joi.object({
+        email:Joi.string().email().required(),
+        password:Joi.string().min(6).required(),
+    })
+    return schema.validate(data)
+}
+
+module.exports={validateRegistration , validateLogin}
