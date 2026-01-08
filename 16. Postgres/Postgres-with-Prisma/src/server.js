1c03415265437e4dcc5fc435fee5a2374abc9244
Yashu Youwaraj <amanyasu@gmail.com>
2026-01-08 10:00:44 +0530

    Postgres with prisma : Introduction, Creating prisma model, Migrating prisma schema

---
+++ 16. Postgres/Postgres-with-Prisma/src/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is now running at port ${PORT}`);
+});
