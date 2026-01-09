2eeeb633b45ec5a8341ac19ea72dada93f97322f
Yashu Youwaraj <amanyasu@gmail.com>
2026-01-09 10:45:23 +0530

    Postgres with prisma : Prometheus and monitoring

--- 16. Postgres/Postgres-with-Prisma/src/server.js
+++ 16. Postgres/Postgres-with-Prisma/src/server.js
@@ -2,13 +2,43 @@ require("dotenv").config();
 const express = require("express");
 const authorRoutes = require("./routes/authorRoutes");
 const bookRoutes = require("./routes/bookRoutes");
+const promClient = require("prom-client");
 
 const app = express();
 app.use(express.json());
 
+// Prometheus metrics setup
+const register = new promClient.Registry();
+promClient.collectDefaultMetrics({ register });
+
+const httpRequestCounter = new promClient.Counter({
+    name:"http_requests_total",
+    help:"Total number of HTTP requests",
+    labelNames:["method","route","status"]
+})
+
+register.registerMetric(httpRequestCounter);
+
+//Middleware to track API requests
+app.use((req,res,next)=>{
+    res.on("finish",()=>{
+        httpRequestCounter.inc({
+            method:req.method,
+            route:req.path,
+            status:res.statusCode
+        })
+    })
+    next()
+})
+
+//Expose the /metrics endpoint for prometheus
+app.get("/metrics",async (req,res) => {
+    res.set("Content-type",register.contentType)
+    res.end(await register.metrics())
+})
+
 app.use("/api/author", authorRoutes);
 app.use("/api/book", bookRoutes);
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is now running at port ${PORT}`));
